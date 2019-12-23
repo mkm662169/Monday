7413e62696784edb698696b4c4db7461a3cba388
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2019-12-23 15:52:27 +0200

    Add project files.

---
+++ Monday/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Monday
+{
+    public class Class1
+    {
+        public void Print(String input)
+        {
+            Console.WriteLine("Hello");
+
+        }
+    }
+}
---
+++ MondayTests/Class1Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monday;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monday.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
